6d2ac006a347395f8bee41ac661e03fabc99558f
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-07-15 14:46:29 -0400

    add dummy data

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState } from 'react'
+
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
+
+export default function ContactList() { 
+
+  const [contacts, setContacts] = useState(dummyContacts)
+  console.log("Contacts: ", contacts)
+  return ( 
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {
+               // Map over data here
+             }
+          </tbody>
+        </table>
+    ); 
+}
+
+
+// const ContactList = () => {
+
+//   return (
+//     <div>
+//       <h2>Contact List</h2>
+//       <ul>
+//         {contacts.map(contact => (
+//           <li key={contact.id}>
+//             <strong>{contact.name}</strong>: {contact.email}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ContactList;
